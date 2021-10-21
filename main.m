474c122026ef4328e5031429373eff006b900969
Jasper Boogers <jasperboogers@gmail.com>
2021-10-21 08:33:05 +0200

    unstretched length L toegevoegd aan V

--- main.m
+++ main.m
@@ -38,23 +38,23 @@ y4d = y3d + L*theta3d*cos(theta3);
 
 %% Energies
 
-% define kinetic energy
+% Kinetic energy
 T1 = 0.5*m1*x1d^2; % wrist
 T2 = 0.5*m2*x2d^2; % lower arm
 T3 = 0.5*m3*x3d^2 + 0.5*m3*y3d^2 + 0.5*(J3g + m3*Lg^2)*theta3d^2; % upper arm
 T = T1 + T2 + T3;
 
-% define potential energy
+% Potential energy
 V1 = 0.5*k1*(x1 - x0)^2; % device-palm
 V2 = 0.5*k2*(x2 - x1)^2; % palm-lower arm
-V3 = 0.5*k3*((x3 - x2)^2 + y3^2) + 0.5*kt3*theta3^2; % lower arm-elbow
-V4 = 0.5*k4*(x4^2 + y4^2) + 0.5*k5*(x4^2 + y4^2); % upper arm-shoulder
+V3 = 0.5*k3*(x3 - x2)^2 + 0.5*kt3*(theta3-pi/2)^2; % lower arm-elbow
+V4 = 0.5*k4*(x4^2 + (y4-L)^2) + 0.5*k5*(x4^2 + (y4-L)^2); % upper arm-shoulder
 V = V1 + V2 + V3 + V4;
 
-% define dissipation
+% Dissipation energy
 D1 = 0.5*c1*(x1d - x0d)^2; % device-palm
 D2 = 0.5*c2*(x2d - x1d)^2; % palm-lower arm
-D3 = 0.5*c3*( (x3d - x2d)^2 + y3d^2) + 0.5*ct3*theta3d^2; % elbow
+D3 = 0.5*c3*(x3d - x2d)^2 + 0.5*ct3*(theta3d)^2; % elbow
 D4 = 0.5*c4*x4d^2 + 0.5*c5*y4d^2; % upper arm-shoulder
 D = D1 + D2 + D3 + D4;
 
@@ -79,11 +79,12 @@ K_eq = subs_eq(K, q, qd, qdd, q_eq, qd_eq, qdd_eq);
 
 Lin_EoM = M_eq*qdd_ + C_eq*qd_ + K_eq*q_;
 
+% possible solution for generalized force vector
+Q2 = simplify(jacobian(V, q) + jacobian(D, qd)).';
+Q2 = subs_eq(Q2, q, qd, qdd, q_eq, qd_eq, qdd_eq);
 
-% possible solutions for generalized force vector
-Q1 = [jacobian(EoM, x0), jacobian(EoM, x0d) ];
-Q2 = simplify(-jacobian(V, q) - jacobian(D, qd)).';
-Q2 = subs_eq(Q2, q, qd, qdd, q_eq, qd_eq, qdd_eq); % does have some weird terms at 4th and 5th index
+%% Eigenmodes
+
 
 %% functions
 
