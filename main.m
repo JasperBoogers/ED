113b14f58469a631bb9d68a7468a3c07a2cef17b
Bas-van-Vliet <Basrvanvliet@gmail.com>
2021-10-27 15:55:41 +0200

    corrected variables and generalized coordinates

--- main.m
+++ main.m
@@ -1,9 +1,9 @@
-clear
+clc,clear all
 
 %% Constants and constraints
 
 % init variables and symbols
-S = struct2cell(load('variables.mat'));
+S = variables();
 syms m1 m2 m3 J3g k1 k2 k3 k4 k5 kt3 c1 c2 c3 c4 c5 ct3 L Lg
 var = [m1; m2; m3; J3g; k1; k2; k3; k4; k5; kt3; c1; c2; c3; c4; c5; ct3; L; Lg];
 
@@ -29,14 +29,17 @@ syms x1dd_ x2dd_ x3dd_ y3dd_ theta3dd_
 q_ = [x1_; x2_; x3_; y3_; theta3_];
 qd_ = [x1d_; x2d_; x3d_; y3d_; theta3d_];
 qdd_ = [x1dd_; x2dd_; x3dd_; y3dd_; theta3dd_];
-
+theta0= pi/2;
 % define constraints and corresponding derivatives
-x4 = x3 + L*cos(theta3);
-x4d = x3d - L*theta3d*sin(theta3);
-y4 = y3 + L*sin(theta3);
-y4d = y3d + L*theta3d*cos(theta3);
+%x4 = x3 + L*cos(theta3);
+x4 = x3 + L*cos(theta3+theta0);
+x4d = x3d - L*theta3d*sin(theta3+theta0);
+y4 = y3 + L*sin(theta3+theta0)-L*sin(theta0);
+y4d = y3d + L*theta3d*cos(theta3+theta0);
 
 %% Energies
+
+%check potential and generalized coordinates
 
 % Kinetic energy
 T1 = 0.5*m1*x1d^2; % wrist
@@ -47,8 +50,8 @@ T = T1 + T2 + T3;
 % Potential energy
 V1 = 0.5*k1*(x1 - x0)^2; % device-palm
 V2 = 0.5*k2*(x2 - x1)^2; % palm-lower arm
-V3 = 0.5*k3*(x3 - x2)^2 + 0.5*kt3*(theta3-pi/2)^2; % lower arm-elbow
-V4 = 0.5*k4*(x4^2 + (y4-L)^2) + 0.5*k5*(x4^2 + (y4-L)^2); % upper arm-shoulder
+V3 = 0.5*k3*(x3 - x2)^2 + 0.5*kt3*(theta3)^2; % lower arm-elbow
+V4 = 0.5*k4*(x4^2 + y4^2) + 0.5*k5*(x4^2 + y4^2); % upper arm-shoulder
 V = V1 + V2 + V3 + V4;
 
 % Dissipation energy
@@ -90,8 +93,9 @@ Mv = double(subs(M_eq,var,S));
 Cv = double(subs(C_eq,var,S));
 [X,eigenfreq]=eig(Kv,Mv);
 omega = sqrt(diag(eigenfreq));
-
+f = omega/(2*pi);
 % Damping
+[XD,omegad]=polyeig(Kv,Cv,Mv);
 % Good approximation/ allowed if:
 % 1 lightly damped
 % 2 Eigenfrequencies well seperated
@@ -99,6 +103,7 @@ Beta = X'*Cv.*X;
 Mu = zeros(5,1);
 Gamma = zeros(5,1);
 Eps = zeros(5,1);
+
 
 for k = 1:size(X)
     Gamma(k) = transpose(X(:,k))*Kv*X(:,k);
@@ -127,4 +132,27 @@ function [res] = subs_eq(mat, v, vd, vdd, v_eq, vd_eq, vdd_eq)
    r1 = subs(mat, v, v_eq);
    r2 = subs(r1, vd, vd_eq);
    res = simplify( subs(r2, vdd, vdd_eq) );
+end
+
+function [S] = variables()
+%% list of all variables
+m1 = 0.45;      % kg, mass of palm
+m2 = 1.15;      % kg, mass of lower arm
+m3 = 1.9;       % kg, mass of upper arm
+J3g = 0.0149;   % kgm^2, mass moment of inertia of upper arm
+k1 = 155.8E3;   % N/m, spring constant of device-palm
+k2 = 23.6E3;    % N/m, spring constant of palm-lower arm
+k3 = 444.6E3;   % N/m, spring constant of lower arm- elbow
+k4 = 415.4E3;   % N/m, spring constant of upper arm-shoulder, x-direction
+k5 = 50.25E3;   % N/m, spring constant of upper arm-shoulder, y-direction
+kt3 = 2;        % Nm/rad, rotational stiffness of elbow
+c1 = 30;        % Ns/m, damping of device-palm
+c2 = 202.84;    % Ns/m, damping of palm-lower arm
+c3 = 500;       % Ns/m, damping of lower arm-elbow
+c4 = 164.59;    % Ns/m, damping of upper arm-shoulder, x-direction
+c5 = 49.99;     % Ns/m, damping of upper arm-shoulder, y-direction
+ct3 = 4.9;      % Nms/rad, rotational damping of elbow
+L = 0.298;      % m, length of upper arm
+Lg = 0.6*L;       % m, distance between elbow and mass center of upper arm
+S = [m1; m2; m3; J3g; k1; k2; k3; k4; k5; kt3; c1; c2; c3; c4; c5; ct3; L; Lg];
 end
